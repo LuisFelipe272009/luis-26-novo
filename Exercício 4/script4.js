be966ed9cb2f96130759629b104fe585d50c894d
LuisFelipe272009 <luis.piekarski@escola.pr.gov.br>
2025-06-26 10:56:43 -0300

    Add files via upload

--- Exercício 4/script4.js
+++ Exercício 4/script4.js
@@ -1,1 +1,12 @@
-let 
+let inscritos = 0;
+let equipes = 0;
+
+function calcular () {
+    inscritos +=1;
+    equipes = parseInt(inscritos / 3);
+    alert ("Número de inscritos" + inscritos + "equipe:" + equipes);
+
+    if(equipes>= 4) {
+        alert("é possivel formar 4 equipes. O campeonato pade iniciar");
+    }
+}
