f2c9d920fb424f1edcffc9f43391d06bb19e02a7
LuisFelipe272009 <luis.piekarski@escola.pr.gov.br>
2025-06-26 09:04:59 -0300

    Add files via upload

---
+++ Exercício 2/script2.js
@@ -0,0 +1,33 @@
+function somar(){
+    let num1 = Number(window.prompt("Digite aqui o promeoro numero"));
+    let num2 = Number (window.prompt("Digite o segundo numero"));
+    let resultado = num1 + num2;
+    alert("O resultado e " + resultado +".")
+}
+
+function subtrair(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro numero"));
+    let num2 = Number(window.prompt("Digite aqui o segundo numero"));
+
+    let resultado = num1 - num2;
+
+    alert("O resultado e " + resultado + ".");
+}
+
+function multiplicar(){
+    let num1 = Number(window.prompt("Digite aqui o promeoro numero"));
+    let num2 = Number (window.prompt("Digite o segundo numero"));
+
+    let resultado = num1 * num2;
+
+    alert("O resultado e " + resultado + ".")
+}
+
+function dividir(){
+    let num1 = Number(window.prompt("Digite aqui o promeoro numero"));
+    let num2 = Number (window.prompt("Digite o segundo numero"));
+
+    let resultado = num1 / num2;
+
+    alert("O resultado e " + resultado + ".")
+}
